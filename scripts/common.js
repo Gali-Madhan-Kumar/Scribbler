341d1f5ff9c4839bcf6812ca3d53391d24504bf8
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-02-18 09:19:53 +0530

    added signin modal

--- scripts/common.js
+++ scripts/common.js
@@ -2,10 +2,27 @@ var modal = document.getElementById("signup_modal");
 var button = document.getElementById("signup_btn");
 var closeModal = document.getElementsByClassName("close")[0];
 
-button.onclick = function() {
+button.onclick = function () {
     modal.style.display = 'flex';
 }
 
-closeModal.onclick = function() {
+closeModal.onclick = function () {
     modal.style.display = 'none';
+}
+var signInModal = document.getElementById("signin_modal");
+var signInButton = document.getElementById("signin_btn");
+var modalClose = document.getElementsByClassName("close")[1];
+var signUp = document.getElementById("signIn_close");
+
+signInButton.onclick = function () {
+    signInModal.style.display = 'flex';
+}
+
+modalClose.onclick = function () {
+    signInModal.style.display = 'none';
+}
+
+signUp.onclick = function () {
+    signInModal.style.display = 'none';
+    modal.style.display = 'flex';
 }
