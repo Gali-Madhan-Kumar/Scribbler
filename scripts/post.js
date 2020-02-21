5822a8de78a10a3732e10fef44306cfe3a6f6ff2
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-02-21 10:48:15 +0530

    now the  user can edit the title and content of the post,added save button

--- scripts/post.js
+++ scripts/post.js
@@ -6,16 +6,33 @@ function openpost(postId) {
 }
 if (document.getElementById('post') != null) {
 	var userName = sessionStorage.getItem('userName');
-var postTitle = sessionStorage.getItem('postTitle');
-var postContent = sessionStorage.getItem('postContent');
-	var template = '<div id="postTitle">' + 
-            '<p>'+ postTitle +'</p>' +
-        '</div>' +
-        '<div id="postDetails">' +
-                '<div id="userName">' +
-            '<p>'+ userName +'</p>' +
-            '<span id="editpost">Edit <i class="fa fa-edit"></i></span>' +
-        '</div>' +
-        '<div id="postContent">' + postContent + '</div>' 
-	document.getElementById('post').innerHTML = template; 
+	var postTitle = sessionStorage.getItem('postTitle');
+	var postContent = sessionStorage.getItem('postContent');
+	var template = '<div id="postTitle">' +
+		'<p>' + postTitle + '</p>' +
+		'</div>' +
+		'<div id="postDetails">' +
+		'<div id="userName">' +
+		'<p>' + userName + '</p>' +
+		'<span id="editpost">Edit <i class="fa fa-edit"></i></span>' +
+		'</div>' +
+		'<div id="postContent">' + postContent + '</div>'
+	document.getElementById('post').innerHTML = template;
 }
+document.getElementsByTagName('span')[0].addEventListener("click", function () {
+	if (document.getElementsByTagName('span')[0].id == 'editpost') {
+		document.getElementById('postTitle').style = 'border: 1px solid red';
+		document.getElementById('postTitle').contentEditable = true;
+		document.getElementById('postContent').style = 'border: 1px solid red';
+		document.getElementById('postContent').contentEditable = true;
+		document.getElementsByTagName('span')[0].setAttribute("id", "savepost");
+		document.getElementsByTagName('span')[0].innerHTML = 'Save <i class="fa fa-save"></i>';
+	} else {
+		document.getElementById('postTitle').contentEditable = false;
+		document.getElementById('postContent').contentEditable = false;
+		document.getElementById('postTitle').style = 'border: 0px';
+		document.getElementById('postContent').style = 'border: 0px';
+		document.getElementsByTagName('span')[0].setAttribute("id", "editpost");
+		document.getElementsByTagName('span')[0].innerHTML = 'Edit <i class="fa fa-edit"></i></span>';
+	}
+});
