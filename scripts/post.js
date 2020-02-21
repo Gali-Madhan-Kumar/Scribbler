bfbd8fc655aa6081176ced8ab6abc9ff53ad63f9
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-02-21 10:43:42 +0530

    displays the post clicked by user inside post page

--- scripts/post.js
+++ scripts/post.js
@@ -4,3 +4,18 @@ function openpost(postId) {
 	sessionStorage.setItem('postContent', document.getElementById(postId.id).getElementsByTagName('p')[2].innerHTML);
 	window.location.href = './post.html';
 }
+if (document.getElementById('post') != null) {
+	var userName = sessionStorage.getItem('userName');
+var postTitle = sessionStorage.getItem('postTitle');
+var postContent = sessionStorage.getItem('postContent');
+	var template = '<div id="postTitle">' + 
+            '<p>'+ postTitle +'</p>' +
+        '</div>' +
+        '<div id="postDetails">' +
+                '<div id="userName">' +
+            '<p>'+ userName +'</p>' +
+            '<span id="editpost">Edit <i class="fa fa-edit"></i></span>' +
+        '</div>' +
+        '<div id="postContent">' + postContent + '</div>' 
+	document.getElementById('post').innerHTML = template; 
+}
